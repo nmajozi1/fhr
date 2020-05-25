0ac065c69f3a620fbe12c8b6b72499390743de23
Ntokozo Majozi (Group) <ntokozo.majozi@global.ntt>
2020-05-25 15:11:19 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReleasesComponent } from './releases/releases.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { NewsComponent } from './news/news.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: ReleasesComponent },
+  { path: 'releases', component: ReleasesComponent },
+  { path: 'artists', component: ArtistsComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'contact', component: ContactsComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ThumbnailsComponent } from './thumbnails/thumbnails.component';
+import { BodyComponent } from './body/body.component';
+import { ReleasesComponent } from './releases/releases.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { NewsComponent } from './news/news.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ArtistComponent } from './artist/artist.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    ThumbnailsComponent,
+    BodyComponent,
+    ReleasesComponent,
+    ArtistsComponent,
+    NewsComponent,
+    ContactsComponent,
+    ArtistComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
